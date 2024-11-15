440cc91e46b31de01d3ee3c278de35acde096534
owlrazum <owlrazum@gmail.com>
2024-11-15 16:59:23 +0600

    private backup

---
+++ 08/Q1/beecrowd/template.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner scan = new Scanner(System.in);  // Create a Scanner object
+        String answer = "";
+        System.out.println(answer);  
+    }
+ 
+}
