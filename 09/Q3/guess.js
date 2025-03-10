2aff3f56b42fab575a9005d71a75e565d2be1e7b
owlrazum <owlrazum@gmail.com>
2025-03-10 08:42:37 +0600

    school

--- 09/Q3/guess.js
+++ 09/Q3/guess.js
@@ -1,4 +1,20 @@
-console.log("Hello world")
-let pi = 3.11412
-console.log(pi.toFixed(2))
-parseInt("2") 
+// npm install prompt-sync
+const prompt = require('prompt-sync')({sigint: true});
+
+let n = Math.floor(Math.random() * 100) + 1
+
+// Math.random() -> 0..0.999999
+// Math.random() * 100 -> 0 .. 99.999999
+while (true) {
+    let guess = parseInt(prompt("Guess a number from 1 to 100"))
+    if (guess > n) {
+        console.log("Try lesser number")
+    }
+    else if (guess < n) {
+        console.log("Try bigger number")
+    }
+    else {
+        console.log("Whooraay! You won!")
+        break
+    }
+}
